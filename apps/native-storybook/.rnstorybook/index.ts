b5fe01f12f8a4a8dc1f918698857e6cc15794f9f
romanbakurov <ronbakurov@gmail.com>
2026-06-30 23:28:10 +0200

    refactor(monorepo): separate Storybook and playground apps

--- apps/native-storybook/.rnstorybook/index.ts
+++ apps/native-storybook/.rnstorybook/index.ts
@@ -3,7 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { view } from './storybook.requires';
 
 const StorybookUIRoot = view.getStorybookUI({
-  shouldPersistSelection: true,
+  shouldPersistSelection: false,
   storage: {
     getItem: AsyncStorage.getItem,
     setItem: AsyncStorage.setItem,
--- apps/native-storybook/index.ts
+++ apps/native-storybook/index.ts
@@ -1,5 +1,4 @@
 import { registerRootComponent } from 'expo';
-
 import StorybookUIRoot from './.rnstorybook';
 
 registerRootComponent(StorybookUIRoot);
